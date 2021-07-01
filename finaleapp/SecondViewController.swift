c58963919bd2cc50d29d6ade0b296329caf71688
Naomibakrr <Naomibaker@gmail.cpm>
2021-07-01 12:27:38 -0400

    lol chickens r cute

--- finaleapp/SecondViewController.swift
+++ finaleapp/SecondViewController.swift
@@ -40,3 +40,59 @@ class SecondViewController: UIViewController {
     */
 
 }
+
+class MyCustomSegue: UIStoryboardSegue {
+    
+    private var selfRetainer: MyCustomSegue? = nil
+
+    override func perform() {
+        destination.transitioningDelegate = self
+        selfRetainer = self
+        destination.modalPresentationStyle = .overCurrentContext
+        source.present(destination, animated: true, completion: nil)
+    }
+}
+
+extension MyCustomSegue: UIViewControllerTransitioningDelegate {
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return Presenter()
+    }
+    
+    public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        selfRetainer = nil
+        return Dismisser()
+    }
+    
+    private class Presenter: NSObject, UIViewControllerAnimatedTransitioning {
+        func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+            return 1.5
+        }
+        
+        func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+            let containerView = transitionContext.containerView
+            let toView = transitionContext.view(forKey: .to)!
+            
+            containerView.addSubview(toView)
+            toView.alpha = 0.0
+            UIView.animate(withDuration: 1.5,
+                           animations: {
+                            toView.alpha = 1.0
+            },
+                           completion: { _ in
+                            transitionContext.completeTransition(true)
+            }
+            )
+        }
+    }
+    
+    private class Dismisser: NSObject, UIViewControllerAnimatedTransitioning {
+        func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+            return 0.2
+        }
+        
+        func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+            // TODO: Animate transition
+        }
+    }
+}
+
